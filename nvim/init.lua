4f22072242025254f2eac52f0034d5ac47a3b076
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-09-14 22:51:02 +0200

    Add more shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,13 +8,38 @@ vim.o.softtabstop = 4
 
 -- Specific overrides
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-	callback = function()
-		vim.bo.expandtab = true -- use spaces
-		vim.bo.tabstop = 2 -- number of spaces per tab
-		vim.bo.shiftwidth = 2 -- number of spaces for auto-indent
-		vim.bo.softtabstop = 2
-	end,
+  pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    vim.bo.expandtab = true -- use spaces
+    vim.bo.tabstop = 2      -- number of spaces per tab
+    vim.bo.shiftwidth = 2   -- number of spaces for auto-indent
+    vim.bo.softtabstop = 2
+  end,
 })
 
 vim.opt.number = true
+
+-- Move current line up/down with Alt+Arrow
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { silent = true })
+
+-- Move selected lines up/down in visual mode
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
+
+-- Tab out
+vim.keymap.set("i", "<Tab>", function()
+  local col = vim.fn.col(".")
+  local line = vim.fn.getline(".")
+  local next_char = line:sub(col, col)
+
+  -- Characters you want to "tab out" of
+  local tabout_chars = { [")"]=true, ["}"]=true, ["]"]=true, [";"]=true, ['"']=true, ["'"]=true }
+
+  if tabout_chars[next_char] then
+    return "<Right>"
+  else
+    return "<Tab>"
+  end
+end, { expr = true, noremap = true })
+
