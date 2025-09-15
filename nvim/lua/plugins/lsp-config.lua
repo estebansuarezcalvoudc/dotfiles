60a1ffeb02619cce7b301a145b259489da7bc020
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-09-15 02:51:39 +0200

    Add tailwind autocompletion

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-      require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "pyright", "ts_ls" } })
+      require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "pyright", "ts_ls", "tailwindcss" } })
     end,
   },
   {
@@ -19,6 +19,7 @@ return {
       lspconfig.lua_ls.setup({})
       lspconfig.pyright.setup({})
       lspconfig.ts_ls.setup({})
+      lspconfig.tailwindcss.setup({})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
