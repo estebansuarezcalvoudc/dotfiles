a89147d0f9af54ea7271c53e659ab8d182cad7f6
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-09-19 16:10:28 +0200

    Format code

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,29 +1,40 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false,                    -- neo-tree will lazily load itself
-  },
-  vim.keymap.set("n", "<C-n>", function()
-    local tree_win = nil
-    for _, win in ipairs(vim.api.nvim_list_wins()) do
-      local buf = vim.api.nvim_win_get_buf(win)
-      local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-      if ft == "neo-tree" then
-        tree_win = win
-        break
-      end
-    end
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					filtered_items = {
+						visible = true,
+						hide_dotfiles = false,
+						hide_gitignored = false,
+					},
+				},
+			})
+		end,
+	},
+	vim.keymap.set("n", "<C-n>", function()
+		local tree_win = nil
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			local buf = vim.api.nvim_win_get_buf(win)
+			local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+			if ft == "neo-tree" then
+				tree_win = win
+				break
+			end
+		end
 
-    if tree_win then
-      vim.api.nvim_win_close(tree_win, true)     -- hide the tree
-    else
-      vim.cmd("Neotree reveal filesystem right") -- show the tree
-    end
-  end, { desc = "Toggle Neo-tree on the right" }),
+		if tree_win then
+			vim.api.nvim_win_close(tree_win, true) -- hide the tree
+		else
+			vim.cmd("Neotree reveal filesystem right") -- show the tree
+		end
+	end, { desc = "Toggle Neo-tree on the right" }),
 }
