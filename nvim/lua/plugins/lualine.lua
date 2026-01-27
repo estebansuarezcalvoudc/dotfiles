c4302473dd41d88af71c1cde85cc6d3e2470fe4c
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-01-27 02:20:34 +0100

    Catpuccin mocha theme for vim status bar

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,26 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "dracula",
-      },
-      sections = {
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+				globalstatus = true,
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+
+		-- Mover la statusline arriba
+		vim.o.laststatus = 3
+		vim.o.winbar = "%{%v:lua.require'lualine'.statusline()%}"
+		vim.o.laststatus = 0
+	end,
 }
