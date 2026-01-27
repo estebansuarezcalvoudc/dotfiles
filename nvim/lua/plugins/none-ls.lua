5b4475ce1a58409d13951c1bc100dd6907fd7b9a
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-01-27 02:23:50 +0100

    Fix some none-ls minor bugs

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -2,21 +2,20 @@ return {
 	"nvimtools/none-ls.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"nvimtools/none-ls-extras.nvim", -- Para eslint_d
 	},
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
-				-- Python (usa las herramientas instaladas por Mason)
-				null_ls.builtins.formatting.ruff, -- Organiza imports + formatea
-				null_ls.builtins.formatting.black, -- Alternativa de formateo
-				null_ls.builtins.diagnostics.ruff, -- Detecta errores
+				-- Python
+				null_ls.builtins.formatting.black,
 
 				-- JavaScript/TypeScript
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.code_actions.eslint_d,
+				require("none-ls.diagnostics.eslint_d"), -- Desde none-ls-extras
+				require("none-ls.code_actions.eslint_d"),
 
 				-- Lua
 				null_ls.builtins.formatting.stylua,
@@ -25,9 +24,14 @@ return {
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format file" })
 
-		-- ✨ Organizar imports (funciona para Python, TS, JS, etc.)
-		vim.keymap.set("n", "<leader>ri", function()
-			vim.lsp.buf.format({ async = false })
+		-- Organizar imports con code actions
+		vim.keymap.set("n", "<leader>go", function()
+			vim.lsp.buf.code_action({
+				apply = true,
+				context = {
+					only = { "source.organizeImports" },
+				},
+			})
 		end, { desc = "Organize imports" })
 	end,
 }
