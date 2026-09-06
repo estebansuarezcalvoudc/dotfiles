ac9d3a66c1bb5f37e7555c6a271b4bf780b49fed
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-09-06 12:40:54 +0200

    Migrate nvim-treesitter config to new main-branch API
    - require('nvim-treesitter').setup/install replaces nvim-treesitter.configs
    - highlight/indent enabled via FileType autocmd with vim.treesitter.start()
    - pin branch = main and lazy = false (new API does not support lazy-loading)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,47 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
 
-    configs.setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "elixir",
-        "heex",
-        "javascript",
-        "typescript",
-        "python",
-        "html",
-        "markdown",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+    -- Instalar parsers (asíncrono; no-op si ya están instalados)
+    require("nvim-treesitter").install({
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "elixir",
+      "heex",
+      "javascript",
+      "typescript",
+      "tsx",
+      "python",
+      "html",
+      "markdown",
+      "markdown_inline",
+    })
+
+    -- Habilitar highlight + indent por filetype (equivalente a los viejos
+    -- highlight = { enable = true } / indent = { enable = true })
+    local filetypes = {
+      "c", "lua", "vim", "help", "query",
+      "elixir", "heex", "javascript", "typescript", "typescriptreact",
+      "python", "html", "markdown",
+    }
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      callback = function(ev)
+        local ok = pcall(vim.treesitter.start)
+        if ok then
+          -- indent experimental (como el viejo indent = { enable = true })
+          vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
     })
   end,
 }
