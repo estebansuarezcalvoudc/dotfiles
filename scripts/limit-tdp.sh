491280ac19de4377fa4a7dc739ac35ac8be31abc
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-09-07 13:44:05 +0200

    Remove unused scripts

--- scripts/limit-tdp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo ryzenadj \
-  --stapm-limit=10000 \
-  --fast-limit=10000 \
-  --slow-limit=10000
-
-sudo cpupower frequency-set -g powersave
-echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost
